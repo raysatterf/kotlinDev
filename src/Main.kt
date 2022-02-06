2097166efd05a2f812f1eb53d14eb1457805a404
Ray Satterfield <raysatterf@gmail.com>
2022-02-06 17:04:20 -0500

    test: added comments to describe what does what

--- src/Main.kt
+++ src/Main.kt
@@ -14,13 +14,13 @@ fun main() {
     val thatNum = 6
     val myHeight = 5.0
     val myLength = 2.0
-    val myRectangle = Rectangle(myHeight,myLength)
+    val myRectangle = Rectangle(myHeight,myLength) // use Rectangle class to assign class output to variable
     val outputNum = sum(thisNum,thatNum) // use sum function to assign function output to variable
     val someNum: Int // declare a variable without initializing it to a string, int, etc
     someNum = 42 // initializing a variable
 
     // print details etc
-    println("the sum of $thisNum and $thatNum is $outputNum")
-    println("The perimeter is ${myRectangle.perimeter}")
-    println(fullName.isEmpty())
+    println("the sum of $thisNum and $thatNum is $outputNum.")
+    println("The perimeter is ${myRectangle.perimeter}.") // here we access the perimeter variable from the Rectangle class
+    println(fullName.isEmpty()) // this evaluates whether the string is empty or not
 }
