6a3ceb759adeaa602494743b004cfed44787e914
Ray Satterfield <raysatterf@gmail.com>
2022-02-13 20:46:36 -0500

    update: refactored randomize.kt, also added more stuff.

--- src/randomize.kt
+++ src/randomize.kt
@@ -1,39 +1,51 @@
 fun main () {
-    val myFirstDice = Dice() // creating our first 'object instance' of our 'Dice' class
-    val diceRoll =  myFirstDice.roll() // initialize 'diceRoll' to the roll 'method' on the myFirstDice 'object'
-    println("Your ${myFirstDice.sides} sided dice rolled a $diceRoll!")
-    myFirstDice.sides = 20 // updating the number of sides on 'myFirstDice'
-    println("Your ${myFirstDice.sides} sided dice rolled ${myFirstDice.roll()}!")
-    // we need to call ANOTHER roll method because 'diceRoll' was initialized before the sides changed
+    val myFirstDice = Dice(6) // creating our first 'object instance' of our 'Dice' class
+    println("Your ${myFirstDice.color} ${myFirstDice.numSides} sided dice rolled a ${myFirstDice.roll()}!")
 
-    // val newCharacter = Seamus()
-    // val printStats = newCharacter.getStats()
+    val mySecondDice = Dice(20)
+    println("Your ${mySecondDice.color} ${mySecondDice.numSides} sided dice rolled a ${mySecondDice.roll()}!")
+
+    val myCoin = Coin()
+    println("You tossed the coin and it landed on ${myCoin.toss()}!")
+     // val newCharacter = Connor()
+     // val printStats = newCharacter.getStats()
 }
 
-class Dice() {
-    var sides = 6 // assigning a 'sides' property to our 'Dice' class
-
+class Dice(val numSides: Int) {
     // this 'method' performs a roll on our dice, we specify that the data returned will be an Int
-    fun roll():Int {
-        val randomNumber = (1..sides).random() // initialize randomNumber by calling the random function on a range
-         return randomNumber // a return statement returns a value from a function
+    var colorNum = 1..6
+    var colorList = arrayOf("red","orange","yellow","green","blue","black")
+    var color = colorList.random()
+    fun roll(): Int {
+        return (1..numSides).random() // a return statement returns a value from a function
     }
 }
 
-class Seamus {
+class Coin() {
+    var coinSide = arrayOf("Heads","Tails")
+    fun toss(): String {
+        return coinSide.random()
+    }
+}
+
+class Connor {
     var weight = 100..300
     var height = 4..8
-    var name = "Seamus, The Salty"
+    var name = "Connor, The Cuddly"
     var perception = 1..10
     var strength = 1..10
-    var manhood = 1..10
+    var manhood = -10..10
+    var fingerLength = 1..10
+    var forHeadHeight = 1..10
 
     fun getStats() {
         println("Character Name: $name")
-        println("Height: ${height.random()} feet tall")
+        println("Height: ${height.random()} ft. tall")
         println("Weight: ${weight.random()} lbs")
+        println("Forehead Height: ${forHeadHeight.random()} inches")
+        println("Finger Length: ${fingerLength.random()} inches")
         println("Perception: ${perception.random()}")
         println("Strength: ${strength.random()}")
-        println("Manhood: ${manhood.random()}")
+        println("Manhood: ${manhood.random()} inches")
     }
 }
