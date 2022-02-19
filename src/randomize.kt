e4456db6d8fa49ba1843daaa9e39905ace725863
Ray Satterfield <raysatterf@gmail.com>
2022-02-18 20:47:29 -0500

    Refactored: following course, deleted chunks, added chunks

--- src/randomize.kt
+++ src/randomize.kt
@@ -1,22 +1,30 @@
+/**
+ * Dice rolling jazz and other bizzzz
+ * 02/2022
+ */
+
 fun main () {
-    val myFirstDice = Dice(6) // creating our first 'object instance' of our 'Dice' class
-    println("Your ${myFirstDice.color} ${myFirstDice.numSides} sided dice rolled a ${myFirstDice.roll()}!")
-
-    val mySecondDice = Dice(20)
-    println("Your ${mySecondDice.color} ${mySecondDice.numSides} sided dice rolled a ${mySecondDice.roll()}!")
-
-    val myCoin = Coin()
-    println("You tossed the coin and it landed on ${myCoin.toss()}!")
-
+    val myFirstDice = Dice(6)
+    val rollResult = myFirstDice.roll()
+    val luckyNumber = 4
+    if (rollResult == luckyNumber) {
+        println("You win!")
+    } else if (rollResult == 1) {
+        println("So sorry! You rolled a 1. Try again!")
+    } else if (rollResult == 2) {
+        println("Sadly, you rolled a 2. Try again!")
+    } else if (rollResult == 3) {
+        println("Unfortunately, you rolled a 3. Try again!")
+    } else if (rollResult == 5) {
+        println("Don't cry! You rolled a 5. Try again!")
+    } else {
+        println("Apologies! You rolled a 6. Try again!")
+    }
 }
 
 class Dice(val numSides: Int) {
-    // this 'method' performs a roll on our dice, we specify that the data returned will be an Int
-    var colorNum = 1..6
-    var colorList = arrayOf("red","orange","yellow","green","blue","black")
-    var color = colorList.random()
-    fun roll(): Int {
-        return (1..numSides).random() // a return statement returns a value from a function
+    fun roll():Int {
+        return (1..numSides).random()
     }
 }
 
